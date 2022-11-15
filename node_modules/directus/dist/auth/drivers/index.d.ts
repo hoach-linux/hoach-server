f7fa090427956488e24e3601002296e41773fbac
hoach-linux <hoachprogrammer@gmail.com>
2022-11-15 16:08:31 +0500

    remove node modules

--- node_modules/directus/dist/auth/drivers/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './local';
-export * from './oauth2';
-export * from './openid';
-export * from './ldap';
-export * from './saml';
--- node_modules/directus/dist/database/helpers/fn/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { FnHelperPostgres as postgres } from './dialects/postgres';
-export { FnHelperPostgres as redshift } from './dialects/postgres';
-export { FnHelperPostgres as cockroachdb } from './dialects/postgres';
-export { FnHelperOracle as oracle } from './dialects/oracle';
-export { FnHelperSQLite as sqlite } from './dialects/sqlite';
-export { FnHelperMySQL as mysql } from './dialects/mysql';
-export { FnHelperMSSQL as mssql } from './dialects/mssql';
